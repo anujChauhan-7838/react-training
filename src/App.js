10581f728414243c14234a1520f5229a7cd0f6aa
Anuj chauhan <anuj07838@gmail.com>
2021-08-24 10:23:01 +0530

    added more cakes

--- src/App.js
+++ src/App.js
@@ -40,6 +40,54 @@ var cke = [
   price:600,
   image:"cake.jpg",
   type:"veg"
+},
+{
+  name:"Chocolate 4",
+  price:600,
+  image:"cake_4.jpg",
+  type:"nveg"
+},
+{
+  name:"Chocolate 6",
+  price:600,
+  image:"cake.jpg",
+  type:"veg"
+},
+{
+  name:"Chocolate 7",
+  price:600,
+  image:"cake_3.jpg",
+  type:"veg"
+},
+{
+  name:"Chocolate 8",
+  price:600,
+  image:"cake.jpg",
+  type:"veg"
+},
+{
+  name:"Chocolate 9",
+  price:600,
+  image:"cake_5.jpg",
+  type:"veg"
+},
+{
+  name:"Chocolate 10",
+  price:600,
+  image:"cake_2.jpg",
+  type:"veg"
+},
+{
+  name:"Chocolate 11",
+  price:600,
+  image:"cake.jpg",
+  type:"veg"
+},
+{
+  name:"Chocolate 12",
+  price:600,
+  image:"cake_4.jpg",
+  type:"veg"
 }
 ];
 
@@ -48,6 +96,7 @@ function App() {
     <div>
    <Nav></Nav>
    <div className="container">
+     <div className="row" style={{"textAlign":"center"}}><strong>Cake List</strong></div>
    <div className="row">
    <Cake cakes={cke}></Cake>
    </div>
--- src/components/Cake.js
+++ src/components/Cake.js
@@ -1,21 +1,18 @@
 
 export default function Cake(props){
-    var elem = '';
-    for(var i=0;i<10;i++){
-      elem = elem + <li>ANuj chauahn</li>
-    }
+   
     const cakes = props.cakes;
     return (
-        cakes.map((cake)=>{
+        cakes.map((cake,index)=>{
             let typeClassName = cake.type=="veg"?"green":"red";
-            return (<div className="col-sm-4" style={{"margin-top":"30px"}}>
-                <div class="card" style={{"width": "18rem","text-align":"center"}}>
+            return (<div className="col-sm-4" style={{"marginTop":"30px"}} key={index}>
+                <div className="card" style={{"width": "18rem","textAlign":"center"}}>
                 <span style={{"color":typeClassName,"padding":"0 .3em"}}>&#9679;&#8414;</span>
-                  <img src={"assests/images/"+cake.image} class="card-img-top" alt="..."  style={{"height":"100px"}} />
-                  <div class="card-body">
-                      <h5 class="card-title">{cake.name}</h5>
-                      <p class="card-text">{"$"+cake.price}</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <img src={"assests/images/"+cake.image} className="card-img-top" alt="..."  style={{"height":"100px"}} />
+                  <div className="card-body">
+                      <h5 className="card-title">{cake.name}</h5>
+                      <p className="card-text">{"$"+cake.price}</p>
+                      <a href="#" className="btn btn-primary">Go somewhere</a>
                   </div>
                   </div>
                   
