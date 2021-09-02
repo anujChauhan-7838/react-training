668231951e8b316b30638e14fea519939110ed66
Anuj chauhan <anuj07838@gmail.com>
2021-09-02 11:49:17 +0530

    change in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <footer className="footer" style={{marginTop:"30px",opacity:1,backgroundColor:"#e4ede6"}}>
+        <footer className="footer" style={{"position": "fixed","left":"0","bottom": "0","width": "100%","backgroundColor":"#e4ede6"}}>
         <div className="container" style={{"textAlign":"center"}}>
           <span className="text-muted">Neosoft Training@2021 All Right Reserved</span>
         </div>
